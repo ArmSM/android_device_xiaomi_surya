468233b5507dde7f5aa623412d2a5294ea009b38
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    sanders: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -14,27 +14,19 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/arrow/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
-function blob_fixup() {
-    case "${1}" in
-    product/lib/libdpmframework.so)
-        patchelf --replace-needed "libcutils.so" "libcutils-v29.so" "${2}"
-        patchelf --add-needed "libcutils.so" "${2}"
-        ;;
-    product/lib64/libdpmframework.so)
-        patchelf --replace-needed "libcutils.so" "libcutils-v29.so" "${2}"
-        patchelf --add-needed "libcutils.so" "${2}"
-        ;;
-    esac
-}
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -64,5 +56,12 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+# Add uhid group for fingerprint service
+FP_SERVICE_RC="$BLOB_ROOT"/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc
+sed -i "s/input/uhid input/" "$FP_SERVICE_RC"
 
 "${MY_DIR}/setup-makefiles.sh"
